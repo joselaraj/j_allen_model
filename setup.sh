b01bab19a6402181c6a2b6d9bbfcff452aab0570
Jose L <josejlaraj@gmail.com>
2026-03-22 21:04:17 -0500

    "adjusted the setup bash script"

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python3.12 -m venv venv
 source venv/bin/activate
-pip install --upgrade pip setuptools
-pip install nfl_data_py --no-deps
+pip install --upgrade pip setuptools 
 pip install -r requirements.txt
