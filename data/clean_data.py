dbe0c8f35fd527c64726b64be5b21eb8bd6967f7
Jose Lara <josejlaraj@gmail.com>
2026-03-22 20:22:12 -0500

    Initial commit

---
+++ data/clean_data.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+
+#import our data 
+df = pd.read_csv('data.csv')
+
+#for our home/away we need to convert to binary for our model 
+df['home_advantage'] = (df['team_abbr'] == df['home_team']).astype(int)
+
+# clean up the data, we dont need all of the columns for our model, we can drop some of them
+to_drop = [
+    'player_display_name',
+    'team_abbr',
+    'home_team',
+    'away_team'
+]
+
+#we know this data is just for Josh Allen so we can drop the player name
+df = df.drop(columns=to_drop)
+
+#get an idea of how many null values there are
+print(df.isnull().sum())
+
+#since players are inside of the dome there isnt any data on wind or temp, so we can set to 0 
+df['wind'] = df['wind'].fillna(0)
+df['temp'] = df['temp'].fillna(df['temp'].median())
+
+
+#add a binary for roof, if it is a dome we can have a 1
+df['roof_binary'] = (df['roof'] == 'dome').astype(int)
+
+df = df.drop(columns='roof')
+
+df = df.rename(columns={'roof_binary': 'dome'})
+
+df.to_csv('clean_data.csv', index=False) 
---
+++ data/player_data.py
@@ -0,0 +1,89 @@
+'''
+Programmer: Jose Lara
+Purpose: Use nfl_data_py to extract data from an active NFL QB and submit the data 
+    into a DB to use for a linear regression model to predict passing yards/attempts
+'''
+#! Import nfl_data_py module for our data
+import nfl_data_py as nfl 
+import pandas as pd
+
+# ?we only want the data from 2020-2025, if we wanted more years we could change this to a loop
+years = [2020,2021,2022,2023,2024,2025]
+
+# import seasonal pro football reference dataframe
+szn_df = nfl.import_ngs_data('passing', years)
+
+# select the columns we want to keep for our model
+clean_up = [
+    'season', 
+    'week',
+    'player_display_name',
+    'team_abbr',
+    'avg_time_to_throw',
+    'pass_yards',   
+    'pass_touchdowns',
+    'completions',
+    'attempts',
+    'interceptions',
+    'aggressiveness',
+    'completion_percentage',
+    'expected_completion_percentage'
+]
+
+#update the season dataframe
+szn_df = szn_df[clean_up]
+
+#import the schedule data, we need this for our home/away and stadium information
+schedule_data = nfl.import_schedules(years) 
+
+#clean up the data, some col worth keeping is roof and wind, can impact passing a lot
+s_clean_up = [
+    'season',
+    'week',
+    'home_team',
+    'away_team',
+    'roof',
+    'away_rest',
+    'home_rest',
+    'wind',
+    'temp'
+]
+
+#put the new columns inside df
+schedule_data = schedule_data[s_clean_up]
+
+'''
+I only want Josh Allen's stats, but, incase down the line 
+    I want to get the data for other players it is going 
+        to be readily available
+'''
+player_name = input('Player name: ') 
+
+#get only the data from the player we choose
+player_df = szn_df[szn_df['player_display_name'].str.contains(player_name, na=False, case=False)]
+
+# merge where team is home
+home_merge = player_df.merge(
+    schedule_data,
+    left_on=['season', 'week', 'team_abbr'],
+    right_on=['season', 'week', 'home_team'],
+    how='inner'
+)
+
+# merge where team is away
+away_merge = player_df.merge(
+    schedule_data,
+    left_on=['season', 'week', 'team_abbr'],
+    right_on=['season', 'week', 'away_team'],
+    how='inner'
+)
+
+# combine both df
+player_df = pd.concat([home_merge, away_merge]).drop_duplicates()
+
+# sort the df by the season/week
+player_df = player_df.sort_values(by=['season', 'week'])
+
+#save df to csv 
+
+player_df.to_csv('data/josh_allen_data.csv', index=False)
